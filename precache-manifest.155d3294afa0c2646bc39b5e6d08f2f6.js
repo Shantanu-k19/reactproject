13423408f668aea6a8b235332cae957a4a684162
Shantanu Khanapure <shantanu.khanapure@gmail.com>
2021-03-13 17:41:52 +0530

    Updates

---
+++ precache-manifest.155d3294afa0c2646bc39b5e6d08f2f6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931ab383edcd123bcb1b98665f7867fd",
+    "url": "/reactproject/index.html"
+  },
+  {
+    "revision": "7bae4b735b4b339656c1",
+    "url": "/reactproject/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7507489c379505145733",
+    "url": "/reactproject/static/css/main.38881c1c.chunk.css"
+  },
+  {
+    "revision": "7bae4b735b4b339656c1",
+    "url": "/reactproject/static/js/2.cccb1f5d.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/reactproject/static/js/2.cccb1f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7507489c379505145733",
+    "url": "/reactproject/static/js/main.06c0b3e8.chunk.js"
+  },
+  {
+    "revision": "3bbad05b96cf21366ef5",
+    "url": "/reactproject/static/js/runtime-main.95317894.js"
+  },
+  {
+    "revision": "e983de446cb1919176792e28f0c6b0b5",
+    "url": "/reactproject/static/media/dribble.e983de44.jpg"
+  },
+  {
+    "revision": "a8859ea048892e557e0a328e43c24624",
+    "url": "/reactproject/static/media/drone.a8859ea0.jpg"
+  },
+  {
+    "revision": "f3ed8258ba35a597298884c5885925f4",
+    "url": "/reactproject/static/media/environment_friendly.f3ed8258.png"
+  },
+  {
+    "revision": "f41dd78d64940fcb5ee9d3f75a830f01",
+    "url": "/reactproject/static/media/fruits.f41dd78d.png"
+  },
+  {
+    "revision": "cd78e7905bba2b050fcc40bd3bd1f83a",
+    "url": "/reactproject/static/media/girl.cd78e790.jpg"
+  },
+  {
+    "revision": "42c6702e36d6ee0c382fb6faf58c4d5b",
+    "url": "/reactproject/static/media/organic.42c6702e.jpg"
+  },
+  {
+    "revision": "ad32194dd3129dd4892559942ffefd81",
+    "url": "/reactproject/static/media/processing.ad32194d.png"
+  },
+  {
+    "revision": "255a324172bf60fc48bc6554ff4e3549",
+    "url": "/reactproject/static/media/unblast.255a3241.jpg"
+  }
+]);
